d6e973c25300b95493bb2a10c78b928995a67227
dragonero2704 <roberto.rudi04@gmail.com>
2024-09-10 01:44:02 +0200

    added search api

--- server/yt-stream.js
+++ server/yt-stream.js
@@ -3,8 +3,13 @@ import {Router} from "express"
 
 const router = Router()
 
-router.get("/search/:query", (req, res)=>{
-  res.send(req.params["query"]);
+router.get("/search/:query", async (req, res)=>{
+  const query = req.params["query"]
+  const result = await ytstream.search(query)
+  .catch(error=>{
+    res.status(500).send(error)
+  })
+  res.send(result)
 });
 
 
